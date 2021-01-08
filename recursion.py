9df42e1cfe6b1dddc7e722d19e7d1a002ba953c2
Leandro Passos <lpkyrius@hotmail.com>
2021-01-08 10:43:32 -0300

    Ajusting some comments

--- recursion.py
+++ recursion.py
@@ -14,18 +14,17 @@ import re
 
 def recursionX(the_expression):
     
-    # let's start with parenthesis
+    # 1 - let's start with: parenthesis
     openning = the_expression.rfind('(') # last occurrence of '('
-    if openning != -1: # doesn't find '('
+    if openning != -1: # there's no '(' anymore
     
-        #second_openning = the_expression.find('(',openning+1)
         closing = the_expression.find(')',openning) 
         result = calculate(the_expression[openning+1:closing])
         result = recursionX(the_expression[:openning]+str(result)+the_expression[closing+1:])
     
     else:
     
-        # now * (multiplication)
+        # 2 -now: * (multiplication)
         multi = the_expression.find('*')
 
         if multi != -1:
@@ -33,7 +32,7 @@ def recursionX(the_expression):
             final_pos = pos+len(re.findall(r'\d+',the_expression)[1])
             result = recursionX(str(calculate(the_expression[:final_pos]))+the_expression[final_pos:])
         else:
-            # finally simple operations (+/-)
+            # 3 - finally simple operations (+/-)
             result = calculate(the_expression)
     return result
     
@@ -52,7 +51,6 @@ def calculate(simple_expression):
     second = simple_expression[pos+1:]
 
     if operator == '+':
-        #return int(simple_expression[0:1])+int(simple_expression[2:3])
         return int(first)+int(second)
     elif operator == '-':
         return int(first)-int(second)
